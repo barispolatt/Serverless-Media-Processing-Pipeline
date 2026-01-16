f03114798f378ecd416f3c829f291a59f194900c
barispolatt1999 <barispolatt1999@gmail.com>
2026-01-16 17:05:18 +0300

    S3 buckets

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,19 @@
 provider "aws" {
   region = "us-west-2" # Oregon
 }
-variable "project_name" { default = "ServerlessMediaProcesing" }
+variable "project_name" { default = "serverlessmediaprocesing-demo" }
 
 resource "random_id" "bucket_suffix" { byte_length = 4 }
+
+# S3 Buckets
+resource "aws_s3_bucket" "input" {
+  bucket = "${var.project_name}-input-${random_id.bucket_suffix.hex}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "output" {
+  bucket = "${var.project_name}-output-${random_id.bucket_suffix.hex}"
+  force_destroy = true
+}
+
+
