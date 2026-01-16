080ca47f18cb841e2615d17b87d6c327fc4fbab2
barispolatt1999 <barispolatt1999@gmail.com>
2026-01-16 17:32:09 +0300

    Configure S3 event notifications to trigger Lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -90,3 +90,22 @@ resource "aws_lambda_function" "processor" {
     }
   }
 }
+
+# S3 Trigger
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.input.id
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.processor.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = ".jpg"
+  }
+  depends_on = [aws_lambda_permission.allow_s3]
+}
+
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.processor.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.input.arn
+}
