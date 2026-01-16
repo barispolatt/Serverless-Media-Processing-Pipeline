307995c4c9cba0ead9b82085727998f2093985c5
barispolatt1999 <barispolatt1999@gmail.com>
2026-01-16 17:05:56 +0300

    DynamoDB

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,4 +16,13 @@ resource "aws_s3_bucket" "output" {
   force_destroy = true
 }
 
-
+# DynamoDB 
+resource "aws_dynamodb_table" "metadata" {
+  name           = "${var.project_name}-meta"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "image_id"
+  attribute {
+    name = "image_id"
+    type = "S"
+  }
+}
